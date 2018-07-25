be1f61e26633ba1701bad87db60e19382c08f4a5
Nikola Paunovic <nikola@applycolo.rs>
2018-07-25 08:14:15 +0200

    adds test

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -38,6 +38,7 @@ defmodule Issues.CLI do
     def procses(user, project, _count) do
         Issues.GithubIssues.fetch(user, project)
         |> decode_response()
+        |> sort_into_descending_order()
     end
 
     def decode_response({:ok, body}), do: body 
@@ -45,5 +46,13 @@ defmodule Issues.CLI do
     def decode_response({:error, error}) do
         IO.puts "Error fet=ching from GitHub: #{error["message"]}"
         System.halt(2)
+    end
+
+    def sort_into_descending_order(list_of_issues) do
+        list_of_issues
+        |> Enum.sort(fn i1, i2 -> 
+            i1["created_at"] >= i2["created_at"]
+        end)
+
     end
 end
